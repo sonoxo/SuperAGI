5582628e3d0f8b4d3c3c6841a896c571b2f1383e
Agens Nihil <60854716+sonoxo@users.noreply.github.com>
2026-08-13 02:03:16 -0400

    Add XuniHub core package

---
+++ superagi/xunihub/__init__.py
@@ -0,0 +1,5 @@
+"""XuniHub core runtime primitives."""
+
+from superagi.xunihub.task_runtime import TaskExecutor, TaskRecord, TaskStatus, TaskStore
+
+__all__ = ["TaskExecutor", "TaskRecord", "TaskStatus", "TaskStore"]
