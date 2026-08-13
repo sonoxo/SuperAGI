876cfe23aee9dd364a2de8b47c5cd6b14ee4bef4
Agens Nihil <60854716+sonoxo@users.noreply.github.com>
2026-08-13 05:01:57 -0400

    Export XuniHub agent execution primitives

--- superagi/xunihub/__init__.py
+++ superagi/xunihub/__init__.py
@@ -1,10 +1,22 @@
 """XuniHub core runtime primitives."""
 
+from superagi.xunihub.agent_execution import (
+    AgentExecutionError,
+    AgentRunResult,
+    AgentTaskHandler,
+    AgentToolLoop,
+    ToolExecutionResult,
+    ToolRegistry,
+)
 from superagi.xunihub.project_memory import ProjectMemoryStore, RepairSession, RepairStatus
 from superagi.xunihub.repair_orchestrator import RepairCoordinator, RepairOutcome
 from superagi.xunihub.task_runtime import TaskExecutor, TaskRecord, TaskStatus, TaskStore
 
 __all__ = [
+    "AgentExecutionError",
+    "AgentRunResult",
+    "AgentTaskHandler",
+    "AgentToolLoop",
     "ProjectMemoryStore",
     "RepairCoordinator",
     "RepairOutcome",
@@ -14,4 +26,6 @@ __all__ = [
     "TaskRecord",
     "TaskStatus",
     "TaskStore",
+    "ToolExecutionResult",
+    "ToolRegistry",
 ]
