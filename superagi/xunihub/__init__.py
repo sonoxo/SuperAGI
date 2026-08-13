ab92867a96f890ddc611219399fa96e1a4f6a295
Agens Nihil <60854716+sonoxo@users.noreply.github.com>
2026-08-13 04:03:59 -0400

    Export XuniHub repair orchestration primitives

--- superagi/xunihub/__init__.py
+++ superagi/xunihub/__init__.py
@@ -1,5 +1,17 @@
 """XuniHub core runtime primitives."""
 
+from superagi.xunihub.project_memory import ProjectMemoryStore, RepairSession, RepairStatus
+from superagi.xunihub.repair_orchestrator import RepairCoordinator, RepairOutcome
 from superagi.xunihub.task_runtime import TaskExecutor, TaskRecord, TaskStatus, TaskStore
 
-__all__ = ["TaskExecutor", "TaskRecord", "TaskStatus", "TaskStore"]
+__all__ = [
+    "ProjectMemoryStore",
+    "RepairCoordinator",
+    "RepairOutcome",
+    "RepairSession",
+    "RepairStatus",
+    "TaskExecutor",
+    "TaskRecord",
+    "TaskStatus",
+    "TaskStore",
+]
